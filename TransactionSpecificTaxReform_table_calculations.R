0d2081e46dff897fdd5cc5d91eaa215caefdf183
tbrenn314 <151448467+tbrenn314@users.noreply.github.com>
2023-11-19 12:15:31 -0500

    Update TransactionSpecificTaxReform_table_calculations.R

--- TransactionSpecificTaxReform_table_calculations.R
+++ TransactionSpecificTaxReform_table_calculations.R
@@ -1,12 +1,13 @@
-############## README ##############
-# script to compute values of tables in
+################################
+# TransactionSpecificTaxReform_table_calculations.R
+# is an R script to compute values of tables in
 # "Transaction-Specific Tax Reform in Three Steps:
 #  The Case of Constructive Ownership"
 # by Thomas J. Brennan and David M. Schizer
 # Columbia Journal of Tax Law, 2023
 #
-# The below commands generate the data for the tables in
-# the paper.  Data frames containing the data are named
+# The commands generate the data for the tables in the
+# paper.  Data frames containing the data are named 
 # dftab1, dftab2, etc.
 #
 # Calculations use the derivmkts library.
@@ -14,9 +15,9 @@
 # The variables tab1fmt, tab2fmt, etc., put the data frames
 # in kable/markdown format using the knitr library.
 #
-# The final lines of the file print the values of each of
+# The final lines of the script print the values of each of
 # tab1fmt, tab2fmt, etc.
-####################################
+################################
 
 # load derivmkts library
 library(derivmkts)
